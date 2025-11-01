1911baa60a96c3b7da801b8bee7e7bf5b90fb5a4
Christian_t <155655274+christian783@users.noreply.github.com>
2025-11-01 21:15:40 +0200

    Add PDF417 decoder functionality

---
+++ PDF417_decoder.py
@@ -0,0 +1,7 @@
+from pdf417decoder import PDF417Decoder
+
+decoder = PDF417Decoder("C:\Users\Christian\OneDrive\Pictures\Screenshots\Screenshot 2025-11-01 205339.png")
+
+for barcode in decoder.barcode_array:
+    text = ''.join([chr(b) if 32 <= b <= 126 else ' ' for b in barcode.data])
+    print(text.strip())
