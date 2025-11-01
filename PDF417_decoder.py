e230867b68e3962a248f15e46e73d84c49f56b2a
Christian_t <155655274+christian783@users.noreply.github.com>
2025-11-01 21:57:49 +0200

    Update PDF417_decoder.py

--- PDF417_decoder.py
+++ PDF417_decoder.py
@@ -1,6 +1,6 @@
 from pdf417decoder import PDF417Decoder
 
-decoder = PDF417Decoder("C:\Users\Christian\OneDrive\Pictures\Screenshots\Screenshot 2025-11-01 205339.png")
+decoder = PDF417Decoder("C:\Users\Christian\OneDrive\Pictures\Screenshots\Screenshot-2025-11-01-205339.png")
 
 for barcode in decoder.barcode_array:
     text = ''.join([chr(b) if 32 <= b <= 126 else ' ' for b in barcode.data])
